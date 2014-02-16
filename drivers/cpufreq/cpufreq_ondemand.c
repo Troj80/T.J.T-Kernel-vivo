5fcd8db36b194640349f365464ade9011fd508bd
Troj80 <alex.troj@gmail.com>
2014-02-16 18:46:55 +0200

    cpufreq: ondemand: add input_boost interface
    Currently Ondemand governor handles any input event
    like touch by scaling the CPU frequency to maximum
    available on the target.
    
    This change adds a new sysfs interface "input_boost"
    whereby the CPU will scale to this frequency on input events.
    The value of this sysfs is user defined so input events
    can now be handled by scaling the CPU to lower frequencies
    than target max.
    
    Change-Id: I5428fd8797c9984b17a66b01a44557f2160e8b68
    Signed-off-by: Dilip Gudlur <dgudlur@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -139,7 +139,9 @@ static struct dbs_tuners {
 	unsigned int boosted;
 	unsigned int freq_boost_time;
 	unsigned int boostfreq;
+	unsigned int input_boost;
 } dbs_tuners_ins = {
+	.input_boost = 0,
 	.up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL,
@@ -309,6 +311,7 @@ show_one(ignore_nice_load, ignore_nice);
 show_one(boostpulse, boosted);
 show_one(boosttime, freq_boost_time);
 show_one(boostfreq, boostfreq);
+show_one(input_boost, input_boost);
 
 static ssize_t show_powersave_bias
 (struct kobject *kobj, struct attribute *attr, char *buf)
@@ -373,6 +376,18 @@ static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
 		return -EINVAL;
 	dbs_tuners_ins.sampling_rate = max(input, min_sampling_rate);
 	current_sampling_rate = dbs_tuners_ins.sampling_rate;
+	return count;
+}
+
+static ssize_t store_input_boost(struct kobject *a, struct attribute *b,
+				const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+	if (ret != 1)
+		return -EINVAL;
+	dbs_tuners_ins.input_boost = input;
 	return count;
 }
 
@@ -589,6 +604,7 @@ define_one_global_rw(powersave_bias);
 define_one_global_rw(boostpulse);
 define_one_global_rw(boosttime);
 define_one_global_rw(boostfreq);
+define_one_global_rw(input_boost);
 
 static struct attribute *dbs_attributes[] = {
 	&sampling_rate_min.attr,
@@ -602,6 +618,7 @@ static struct attribute *dbs_attributes[] = {
 	&boostpulse.attr,
 	&boosttime.attr,
 	&boostfreq.attr,
+	&input_boost.attr
 	NULL
 };
 
@@ -902,14 +919,19 @@ static void dbs_refresh_callback(struct work_struct *unused)
 		goto bail_incorrect_governor;
 	}
 
-	if (policy->cur < policy->max) {
+	if (dbs_tuners_ins.input_boost)
+		target_freq = dbs_tuners_ins.input_boost;
+	else
+		target_freq = policy->max;
+
+	if (policy->cur < target_freq) {
 		/*
 		 * Arch specific cpufreq driver may fail.
 		 * Don't update governor frequency upon failure.
 		 */
-		if (__cpufreq_driver_target(policy, policy->max,
+		if (__cpufreq_driver_target(policy, target_freq,
 					CPUFREQ_RELATION_L) >= 0)
-			policy->cur = policy->max;
+			policy->cur = target_freq;
 
 		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&this_dbs_info->prev_cpu_wall);
